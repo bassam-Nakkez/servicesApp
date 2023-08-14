d62e29caf4f60172e8ba1e68f0703d4bf44bb84b
Bassam-Nakkez <bassamnakkez@gmail.com>
2023-08-14 17:38:36 +0300

    add helper functions
    sendError function
    sendResponse function

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,4 +9,48 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+
+    public function sendRespons ( $result , $messages  )
+    {
+    $respons =
+    [
+        'success' => true ,
+        'data' =>  $result ,
+        'message' => $messages,
+    ];
+
+    return response()->json($respons , 200);
+    }
+
+
+    public function sendError ( $error ,  $errorMessages = [] , $code = 404)
+    {
+
+        $respons =
+        [
+            'success' => false ,
+            'errors'  => $error ,
+        ];
+
+        return response()->json($respons , $code);
+    }
+
+
+    public function sendErrors (  $errorMessages = [] , $code = 404)
+    {
+        $respons =
+        [
+            'success' => false ,
+            'errors'  => 'Error!' ,
+        ];
+
+        if(!empty($errorMessages)) // empty
+        {
+            $respons['errors'] = $errorMessages;
+            return $respons;
+        }
+
+        return response()->json($respons , $code);
+    }
 }
